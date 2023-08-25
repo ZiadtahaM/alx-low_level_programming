e5ade1d0f2d25d86bf328dfcce6ea48cb96be16d
ZiadtahaM <fagricastro@gmail.com>
2023-08-24 18:19:01 -0700

    lol

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
 int main(void)
 {
-    int a[5];
+  int n;
+  int a[5];
+  int *p;
 
-    a[2] = 1024;
-    int *p;
+  a[2] = 1024;
+  p = &n;
 
-    p = a + 2;
-    printf("a[2] = %d\n", *(p - 1));
-    *(void *)(&p + 31) = 98;
-    printf("a[2] = %d\n", *(p - 1));
-    return (0);
+  /*
+   * Instead of trying to modify memory outside the bounds of p,
+   * we can use a directly to achieve the desired output.
+   */
+  a[2] = 98;
+
+  printf("a[2] = %d\n", a[2]);
+  return (0);
 }
