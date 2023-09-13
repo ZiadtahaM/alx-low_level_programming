9fed3157a64e31e0e47ea281dfe0eae4767bc185
ZiadtahaM <fagricastro@gmail.com>
2023-09-13 14:50:39 -0700

    l

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -6,11 +6,15 @@
  * @b: number of bytes to allocate
  *
  * Return: a pointer to the allocated memory
- */}
-void *malloc_checked(unsigned int b);
+ */
+void *malloc_checked(unsigned int b)
 {
-	int *n = malloc(b);
-	if (n == 0)
+	void *ptr;
+
+	ptr = malloc(b);
+
+	if (ptr == NULL)
 		exit(98);
-	return (n);
+
+	return (ptr);
 }
